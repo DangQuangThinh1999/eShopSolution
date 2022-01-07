85e6b5645597ed711e5e2f05c57c35b354690cce
THỊNH ĐẶNG QUANG <93868132+DangQuangThinh1999@users.noreply.github.com>
2022-01-07 21:06:14 +0700

    Using Fluent Validation

--- eShopSolution.ViewModels/System/Users/LoginRequest.cs
+++ eShopSolution.ViewModels/System/Users/LoginRequest.cs
@@ -11,7 +11,7 @@ namespace eShopSolution.ViewModels.System.Users
     {
         public string UserName { get; set; }
 
-        public string Passwrod { get; set; }
+        public string Password { get; set; }
 
         public bool RememberMe { get; set; }
     }
