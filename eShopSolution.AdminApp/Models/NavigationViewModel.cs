3d3fe5a9952328d01013334d821c4f760754c087
THỊNH ĐẶNG QUANG <93868132+DangQuangThinh1999@users.noreply.github.com>
2022-02-07 21:05:59 +0700

    admin_token

---
+++ eShopSolution.AdminApp/Models/NavigationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace eShopSolution.AdminApp.Models
+{
+    public class NavigationViewModel
+    {
+        public List<SelectListItem> Languages { get; set; }
+
+        public string CurrentLanguageId { get; set; }
+
+        public string ReturnUrl { set; get; }
+    }
+}
---
+++ eShopSolution.ApiIntegration/UserApiClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using eShopSolution.ViewModels.System.Users;
+using Newtonsoft.Json;
+
+namespace eShopSolution.ApiIntegration
+{
+    public class UserApiClient : IUserApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> Authenticate(LoginRequest request)
+        {
+            var json = JsonConvert.SerializeObject(request);
+            var httpContent = new StringContent(json, encoding: Encoding.UTF8, "application/json");
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://localhost:7193");// localhost của backend API
+            var response = await client.PostAsync("api/users/authenticate", httpContent);
+
+            var token = await response.Content.ReadAsStringAsync();
+            return token;
+        }
+    }
+}
